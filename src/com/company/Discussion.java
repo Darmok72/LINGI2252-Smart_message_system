6253bdc16f4f0f1a4b1133cab66e18559a2bc237
Darmok72 <johan.vanroy@student.uclouvain.be>
2023-10-18 10:17:29 +0200

    basic template

---
+++ src/com/company/Discussion.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Discussion {
+
+    public void find_message(String s){}
+
+    public void send(Message m){}
+
+    public void receive(){}
+}
---
+++ src/com/company/User.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class User {
+    public String email;
+    public String name;
+
+    public void add_friend(String s){}
+
+    public void remove_friend(String s){}
+
+    public void set_nickname(String s){}
+
+}
