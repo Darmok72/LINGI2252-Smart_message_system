fbd0364c82a0fec070d9312da802ba730bd440ea
Darmok72 <johan.vanroy@student.uclouvain.be>
2023-10-26 11:57:04 +0200

    better pattern matching 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,38 +8,62 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
+
         /*
         selon le type de feature activé des commandes supplémentaires seront accessibles. si la feature n'est pas activée
         et que la commande est appelée alors il y aura un message d'erreur
 
          c user_name :  create user with name user_name
-         a user_name friend_name r_name : add friend friend_name to user user_name
+         a user_name friend_name user_name : add friend friend_name to user user_name
          s user_name1 user_name2 message : send a from user_name1 to user_name2
+         exit : end and close app
 
          // if the message send is the first we should create a new discussion, if not just add the message to the discussion
 
 */
-        Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a command: ");
-        String input = scanner.nextLine();
 
-        Pattern pattern = Pattern.compile("test\\s+(\\w+)\\s+(\\w+)\\s+\"([^\"]+)\"");
-        Matcher matcher = pattern.matcher(input);
+            while(true) {
+                System.out.print("Enter a command: ");
+                String input = scanner.nextLine();
 
-        if (matcher.matches()) {
-            String user1 = matcher.group(1);
-            String user2 = matcher.group(2);
-            String message = matcher.group(3);
-
-            System.out.println(" user1: " + user1 + ", user2: " + user2 + ", message: " + message);
-        } else {
-            System.out.println("Invalid command format.");
+                //    s user_name1 user_name2 message : send a from user_name1 to user_name2
+                if (input.startsWith("s")) {
+                    sendMessage(input);
+                }
+                //exit and close
+                else if (input.startsWith("exit")) {
+                    break;
+                }
+                // else if (input.startsWith(aaa"test2")) {
+                //  processDeleteCommand(input);}
+                else {
+                    System.out.println("Invalid command");
+                }
+            }
+            scanner.close();
         }
 
-        scanner.close();
+    //s user_name1 user_name2 message : send a from user_name1 to user_name2
+        public static void sendMessage(String input) {
+            Pattern pattern = Pattern.compile("s\\s+(\\w+)\\s+(\\w+)\\s+\"([^\"]+)\"");
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.matches()) {
+                String user1 = matcher.group(1);
+                String user2 = matcher.group(2);
+                String message = matcher.group(3);
+
+                System.out.println("Sending from: " + user1 + " to: " + user2 + " with message: " + message);
+            } else {
+                System.out.print("Invalid add command format.");
+            }
+        }
+
     }
 
-}
+
+
 
